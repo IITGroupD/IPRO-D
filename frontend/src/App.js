702acd6a722ecefeb66d540aff57afe0d9ff6e20
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-02-21 18:44:32 -0600

    refactor: remove unnecessary code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import books from './modules/books'
-
+import React, { useState, useEffect } from 'react'
 function App() {
+  const [ bookList, setBookList] = useState([])
+  
+  useEffect(() => {
+    books.getAll()
+      .then(books => setBookList(books))
+  }, [])
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(bookList)}
     </div>
   );
 }
