b162d0f72d637b340d46f4704f17981a15f2e0a6
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-02-22 21:11:03 -0600

    feat: add post handler

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const app = express()
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
-
 require('dotenv').config()
 
 const Book = require('./models/books')
@@ -12,6 +11,25 @@ app.get('/api/books', (req, res) => {
   Book.find({})
     .then(books => {
       res.json(books)
+    })
+    .catch(error => console.log(error))
+})
+
+app.post('/api/books', (req, res) => {
+  const body = req.body
+  const book = new Book({
+    sellerName: body.sellerName,
+    bookName: body.bookName,
+    price: body.price,
+    author: body.author,
+    year: body.year,
+    location: body.location,
+    quality: body.quality,
+    image: body.image
+  })
+  book.save()
+    .then(savedBook =>{
+      res.json(savedBook)
     })
     .catch(error => console.log(error))
 })
