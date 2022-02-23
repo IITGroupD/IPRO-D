cfe88bdbd1d055a8f23d7d9cc46e9fc838382583
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-02-22 21:03:23 -0600

    refactor: fix key error

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -2,27 +2,17 @@ import Listing from './Listing'
 import React from 'react'
 
 export default function Display(props){
-
-    const listDisplay=(props)=>{
-        const {books}=props
-        if(books.length>0){
-        return(
-            books.map((book,index)=>{
-    return(
-        <div className="Filter">
-            <Listing book={book} key={book.id} />
+    const {books} = props
+    let listings;
+    if (books.length > 0) {
+        listings = books.map(book => <Listing book={book} key={book.id}/>)
+    } else {
+        listings = <h3>No listings!</h3>
+    }
+    return (
+        <div className='filter'>
+            {listings}
         </div>
-    )}
-        )
-        )
-    }
-    else{
-        return(<h3>No Listings</h3>)
-    }
-}
-return(
-    <>
-    {listDisplay(props)}
-    </>
-)
+    )
+
 }
