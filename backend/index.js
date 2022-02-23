7b4d7045575b7800211f0af9e263916b1b07c469
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-02-22 21:11:19 -0600

    chore: install morgan

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+var morgan = require('morgan')
+app.use(morgan('tiny'))
 const cors = require('cors')
 app.use(cors())
 require('dotenv').config()
