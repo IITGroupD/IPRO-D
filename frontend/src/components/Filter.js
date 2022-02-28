25e2d5d5a757e8c54575c08adc479d01e79ef6fe
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-02-27 18:26:19 -0600

    refactor: reorganize components

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Filter.css'
-export default function Filter(){
+import FilterSlider from './FilterSlider';
+export default function Filter(props){
+    const { handleChange, handleChangeDistance } = props
     return(
         <div className="filter">
-            
+            <FilterSlider 
+                initValue = {0}
+                minValue = {-100}
+                maxValue = {100}
+                handleChange = {handleChange}
+                />
+            <FilterSlider 
+                initValue = {0}
+                minValue = {-100}
+                maxValue = {100}
+                handleChange = {handleChangeDistance}
+            />
         </div>
     );
 }
