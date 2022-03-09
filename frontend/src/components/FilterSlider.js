90d068714670212a9b66caa557f6559c218639f5
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-03-08 19:29:38 -0600

    refactor: remove spaces

--- frontend/src/components/FilterSlider.js
+++ frontend/src/components/FilterSlider.js
@@ -3,7 +3,7 @@ import './FilterSlider.css'
 // need to import the component of listing
 const FilterSlider = ({initValue, minValue, maxValue, handleChange}) => {
     return (
-        <>
+        <div>
             {minValue}
             <input
             type = "range"
@@ -14,8 +14,7 @@ const FilterSlider = ({initValue, minValue, maxValue, handleChange}) => {
             //need to add value = {listing value} here
             onMouseUp = {handleChange}/>
             {maxValue}
-
-        </>
+        </div>
     );
 };
 
