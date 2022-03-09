535a1d1b81a153eb0a9400510ac4ffa1ec78c9bf
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-03-08 19:28:41 -0600

    refactor: remove price transformation

--- backend/models/books.js
+++ backend/models/books.js
@@ -40,7 +40,6 @@ mongoose.connect(url)
   bookSchema.set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
-      returnedObject.price = returnedObject.price.toString()
       delete returnedObject._id
       delete returnedObject.__v
     }
