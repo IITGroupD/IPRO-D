aee6bc8280b569075216693eb6acf524846daefc
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-04-04 03:24:46 -0500

    feat: add react-google-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,13 @@ import SearchBar from './components/SearchBar';
 import Filter from './components/Filter';
 import books from './modules/books';
 import React, { useState, useEffect } from 'react';
-
+import GoogleLogin from 'react-google-login'
 
 function App() {
   const [bookList, setBookList] = useState([])
-  const [value, setValue] = useState(100);
-  const [distance, setDistance] = useState(100);
-  
-  
-
+  const [value, setValue] = useState(100)
+  const [distance, setDistance] = useState(100)
+  const [loginData, setLoginData] = useState(null)
 
   useEffect(() => {
     books.getAll()
@@ -27,14 +25,47 @@ function App() {
   const handleChangeDistance = event =>{
     setDistance(parseInt(event.target.value))
   }
+
+  const handleLogin = async (googleData) => {
+    setLoginData({
+      name: googleData.Du.tf,
+      email: googleData.Du.tv
+    })
+  }
+
+  const handleLogOut = () => {
+    setLoginData(null)
+  }
+
+  const handleFailure = (result) => {
+    alert('Failed to login')
+  }
   
   return (
     <div className="grid-wrapper">
-      {console.log(bookList)}
       <img src = {'/bb.png'} alt = "Logo"/>
       <Post setBookList = {setBookList}
             bookList = {bookList}/>
       <SearchBar />
+      {loginData 
+        ? 
+          (
+            <div>
+              <h3>You logged in as {loginData.email}</h3>
+              <button onClick={handleLogOut}>Logout</button>
+            </div>
+          )
+        : 
+          (
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              buttonText="Log in with Google"
+              onSuccess={handleLogin}
+              onFailure={handleFailure}
+              cookiePolicy={'single_host_origin'}>   
+            </GoogleLogin>
+          )
+      }
       <Filter 
         handleChangeDistance={handleChangeDistance}
         handleChange={handleChange}
